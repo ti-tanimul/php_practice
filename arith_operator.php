96bd15ba8bb698e79efb911b262bb45f8277492f
Tanimul <tanimul.cse@gmail.com>
2023-03-04 10:17:19 +0600

    php practice file add

---
+++ arith_operator.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    /* Areathmetic Operator
+    $a = 13;
+    $b = 5;
+    $c = $a + $b * 2;
+    echo $c; */
+
+    /* Assignment Operator
+    $a = 60;
+    $b = 12;
+    $a += $b;
+    echo $a; */
+
+    // Comparison Operators
+
+    $var = 100;
+    $var1 = 10;
+
+    var_dump ()
+
+
+
+    ?>
+</body>
+</html>
---
+++ datatype.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // $a = "Tanimul";
+    // $b = 12345;
+    // $c = 5.56;
+    // $d = true;
+    // $e = null;
+    // $f = array("shakil", 123, 5.43, false, null);
+
+    // echo $c; 
+    // var_dump($f [3]);
+
+    $Tanimul = "Patuakhali";
+    $Sagor = "chadpur";
+    $Chadpur = array("Shagor", "Tanjir", "Shanta", 1232, 3.455, true);
+
+    echo "$Sagor, $Tanimul";
+    var_dump($Chadpur[5]);
+
+    ?>
+
+</body>
+</html>
